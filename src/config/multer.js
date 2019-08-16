cf84341164aa4985a311010f676c139d36ddd8a0
Romulo Rocha <romulorocha063@gmail.com>
2019-08-16 18:09:53 -0300

    Storage em S3

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,11 +1,21 @@
-import multer from 'multer';
+import multerS3 from 'multer-s3';
 import crypto from 'crypto';
-import { extname, resolve } from 'path';
+import AWS from 'aws-sdk';
+import { extname } from 'path';
+import s3Config from './s3';
+
+AWS.config.update(s3Config);
+const s3 = new AWS.S3({});
 
 export default {
-  storage: multer.diskStorage({
-    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    filename: (req, file, cb) => {
+  storage: multerS3({
+    s3,
+    bucket: 'gobarber-bootcamp',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
       crypto.randomBytes(16, (err, res) => {
         if (err) return cb(err);
 
@@ -14,3 +24,16 @@ export default {
     },
   }),
 };
+
+// export default {
+//   storage: multer.diskStorage({
+//     destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+//     filename: (req, file, cb) => {
+//       crypto.randomBytes(16, (err, res) => {
+//         if (err) return cb(err);
+
+//         return cb(null, res.toString('hex') + extname(file.originalname));
+//       });
+//     },
+//   }),
+// };
